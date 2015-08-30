b5af5897baa28a66b95813359c1835e42d394d88
Duncan Fermor <duncan@elitemail.org>
2015-08-30 21:39:45 +0100

    add radio and remove regular updates

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 /*jslint indent: 2 */
 /*global m, _ */
-/*global puts, print, to_a */
+/*global puts, print, to_a, append */
 /*global svg_builder, circle */
 /*global document */
+/*global radio */
+
 "use strict";
 
 var actions = [{x: 50, y: 50, type: "point"}];
@@ -25,15 +27,39 @@ function build_components(actions) {
   return components;
 }
 
+function background_rect() {
+  return m(
+    "rect[height='500px'][width='500px']",
+    {"class":"bg", "onclick": "on_click(event)" }
+  );
+}
+
 function build_main() {
-  return m("svg[height='500px'][width='500px']"
-    , m("rect[height='500px'][width='500px']", {"class":"bg"})
+  return m(
+    "svg[height='500px'][width='500px']"
+    , background_rect()
     , build_components(actions)
   );
 }
 
-setInterval(function() {
+function on_click(evt) {
+  radio("main-click").broadcast(evt);
+}
+
+function add_point(evt) {
+  append(
+    actions,
+    {x: evt.offsetX, y: evt.offsetY, type: "point"}
+  );
+}
+
+function render() {
   m.render(document.body, [
     build_main()
   ]);
-}, 30);
+}
+
+radio("main-click").subscribe(add_point);
+radio("main-click").subscribe(render);
+
+render();
